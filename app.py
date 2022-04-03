3f1e5f49e936b54c71f96068f8383c06a8e04f12
Sani <65928582+sghulamani@users.noreply.github.com>
2022-04-02 23:43:20 -0500

    create_accounts kinda working

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
 import requests
 import os
 import json
+import pyodbc
 
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
 
+load_dotenv(dotenv_path="./config.py")
 app = Flask(__name__, static_url_path='/static')
+
+SERVER_NAME = os.getenv("SERVER_NAME")
+SERVER_USERNAME = os.getenv("SERVER_USERNAME")
+SERVER_PASSWORD = os.getenv("SERVER_PASSWORD")
+
+server = SERVER_NAME + '.database.windows.net'
+database = 'test'
+username = SERVER_USERNAME
+password = SERVER_PASSWORD
+driver= '{ODBC Driver 17 for SQL Server}'
 
 
 @app.route('/')
 def landingPage():
     return render_template('create_account.html')
 
+
+@app.route('/_user_info', methods=['POST'])
+def user_info():    
+    data = request.get_json()
+    print(data)
+    with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT TOP 3 first_name,email FROM user")
+            row = cursor.fetchone()
+            while row:
+                print (str(row[0]) + " " + str(row[1]))
+                row = cursor.fetchone()
+
+    return ('', 204)
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost")
