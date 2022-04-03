ef1e3272dd16d7798bce827a14b0f5ff8bc6fdd9
Sani <65928582+sghulamani@users.noreply.github.com>
2022-04-03 06:55:07 -0500

    done diddly did it

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 import pyodbc
 
 from dotenv import load_dotenv
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, redirect, url_for, jsonify
 
 load_dotenv(dotenv_path="./config.py")
 app = Flask(__name__, static_url_path='/static')
@@ -22,19 +22,58 @@ driver= '{ODBC Driver 17 for SQL Server}'
 
 @app.route('/')
 def landingPage():
-    return render_template('create_account.html')
+    return render_template('login.html')
 
 
 @app.route('/_user_info', methods=['POST'])
 def user_info():    
     data = request.get_json()
     print(data)
+    
     with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
         with conn.cursor() as cursor:
             cursor.execute("insert into [dbo].[User](first_name, last_name, email, password) values ('{}', '{}', '{}', '{}')".format(data["first_name"], data["last_name"], data["email"], data["password"]))
             conn.commit()
 
+            cursor.execute("select user_id from [dbo].[User] where email='{}' and password='{}'".format(data["email"], data["password"]))
+            user_id = cursor.fetchall()[0][0]
+
+            print("user_id: ")
+            print(user_id)
+
+            availablility = data["availability"]
+            for i, hour in enumerate(availablility):
+                for j, day in enumerate(availablility[i]):
+                    if availablility[i][j]==1:
+                        print(j, 8+i)
+                        cursor.execute("insert into [dbo].[Schedule](user_id, day_of_week, block_time) values ('{}', '{}', '{}')".format(user_id, j, 8+i))
+                        conn.commit()
+
     return ('', 204)
+
+@app.route('/_login_info', methods=['POST'])
+def login_info():    
+    data = request.get_json()
+    print(data)
+    with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("select count(*) from [dbo].[User] where email='{}' and password='{}'".format(data["email"], data["password"]))
+            
+            count = cursor.fetchall()[0][0]
+        
+            if count==0:
+                return 'false'
+            else:
+                print("send")
+                return jsonify({"redirect": "/edit_preferences"})
+
+@app.route('/edit_preferences', methods=['GET','POST'])
+def edit_preferences():
+    return render_template('edit_preferences.html')
+
+@app.route('/create_account', methods=['GET','POST'])
+def create_account():
+    return render_template('create_account.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost")
---
+++ static/scripts/schedule_algo.py
@@ -0,0 +1,50 @@
+import enum
+import os
+import pyodbc
+import time
+
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="../../config.py")
+
+SENDGRID_KEY = os.getenv("SENDGRID_KEY")
+SERVER_NAME = os.getenv("SERVER_NAME")
+SERVER_USERNAME = os.getenv("SERVER_USERNAME")
+SERVER_PASSWORD = os.getenv("SERVER_PASSWORD")
+
+server = SERVER_NAME + '.database.windows.net'
+database = 'test'
+username = SERVER_USERNAME
+password = SERVER_PASSWORD
+driver= '{ODBC Driver 17 for SQL Server}'
+
+days_arr = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT distinct([activities]) FROM [dbo].[Activities]")
+            activites = cursor.fetchall()
+            #print(activites)
+
+            for i in enumerate(activites):
+                for j in range(13):
+                    cursor.execute("SELECT s.*, email, activities from [dbo].[Schedule] s JOIN [dbo].[USER] u on s.user_id = u.user_id JOIN [dbo].[Activities] a on a.user_id=s.user_id WHERE activities = '{}' and block_time = {}".format(i[1][0],j))
+                    data = cursor.fetchall()
+                    if data:
+                        for k, val in enumerate(data):
+                            message = Mail(
+                                from_email='sghulamani@crimson.ua.edu',
+                                to_emails = data[k][3],
+                                subject = data[k][4] + ' Activity Found',
+                                html_content='<strong>On ' + days_arr[data[k][1]] + ' from ' + str(data[k][2]) + ' to ' + str(data[k][2]+1) + ' </strong>')
+                            try:
+                                sg = SendGridAPIClient(SENDGRID_KEY)
+                                response = sg.send(message)
+                                print(response.status_code)
+                                print(response.body)
+                                print(response.headers)
+                            except Exception as e:
+                                print(e.message)
